2bce2d1edfd81446378e61ffc5d9cb0922212400
bytebitgo <bytebit@foxmail.com>
2025-05-13 16:46:38 +0800

    修改app.py中的端口号，从5001更改为5000以优化服务配置。更新版本号并修改changelog.md和readme.md以反映这些变更。

--- app.py
+++ app.py
@@ -921,7 +921,7 @@ if __name__ == '__main__':
         print("准备启动worker线程", flush=True)
         t = threading.Thread(target=ai_worker, daemon=True)
         t.start()
-    app.run(host='0.0.0.0', port=5001, debug=False, use_reloader=False) 
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False) 
 
 
 
